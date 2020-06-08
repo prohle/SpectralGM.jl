9218a854e7945939889a9b1708ec0dbc6e413b1c
Huong Yen Le <prohle@gmail.com>
2020-06-08 13:05:18 -0400

    test

--- src/SpectralGM.jl
+++ src/SpectralGM.jl
@@ -1,5 +1,23 @@
 module SpectralGM
 
-# Write your package code here.
+  function __init__()
+  end
+
+  using Revise
+  using SuiteSparse
+  using DataStructures
+  using SparseArrays
+  using Random
+  using LinearAlgebra
+  using Statistics
+  using Printf
+
+  using DelimitedFiles
+
+  using Plots
+
+  include("Calculator.jl")
+
+  export test
 
 end
